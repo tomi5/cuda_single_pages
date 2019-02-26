526a268e947bf2b26110300558d5d2aa584d65e1
Tomasz <34981728+tomi5@users.noreply.github.com>
2019-02-26 14:14:51 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -3,18 +3,24 @@ const nav = document.querySelector('.nav');
 const navItemsArray = document.querySelectorAll('.nav__list__link');
 const menuLogo = document.querySelector('.logo__container');
 
+
 function toggleActive() {
  burger.classList.toggle('active');
  nav.classList.toggle('active');
  menuLogo.classList.toggle('active');
 }
 
+console.log(nav.getAttribute.length)
+console.log(nav.getAttribute.class)
+
 burger.addEventListener('click', toggleActive);
 
-// if (nav.attributes = 'active') {
 
-//  for (i = 0, i < navItemsArray.length, i++) {
-//   navItemsArray[i].addEventListener('click', toggleActive);
-//   console.log('ok')
-//  }
-// }
+
+if (nav.classList.contains('active')) {
+navItemsArray.forEach(function(item) {
+	item.addEventListener('click', toggleActive)
+})
+}
+
+
